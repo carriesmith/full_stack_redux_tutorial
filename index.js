7a181d30da88ea470378653379780fb188f811e6
carriesmith <carrie.elizabeth@gmail.com>
2015-12-19 18:51:01 -0500

    add socket io and initial data, begin vote

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import makeStore from './src/store';
+import startServer from './src/server'
 
 export const store = makeStore();
+startServer(store);
+
+// Load items to vote on.
+store.dispatch({
+	type: 'SET_ENTRIES',
+	entries: require('data/entries.json')
+});
+// Initiate voting after items loaded by calling NEXT action
+store.dispatch({type: 'NEXT'});
