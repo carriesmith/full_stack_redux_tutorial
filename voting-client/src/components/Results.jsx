f070333e390f1dad9f5e1c29c374ca875d81d659
carriesmith <carrie.elizabeth@gmail.com>
2015-12-24 21:01:01 -0500

    add tally to results screen

--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -7,11 +7,21 @@ export default React.createClass({
 	getPair: function(){
 		return this.props.pair || [];
 	},
+	getVotes: function(entry){
+		console.log(this.props);
+		if (this.props.tally && this.props.tally.has(entry)){
+			return this.props.tally.get(entry);
+		}
+		return 0;
+	},
 	render: function(){
 		return <div className = "results">
 			{this.getPair().map(entry => 
 				<div key={entry} className="entry">
 					<h1>{entry}</h1>
+					<div className="voteCount">
+						{this.getVotes(entry)}
+					</div>
 				</div>
 				)}
 		</div>
